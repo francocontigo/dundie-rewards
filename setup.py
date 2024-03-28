7e47e7533c0a4d7313e3de1f27a63a13652ea6a5
francocontigo <ricardoaugustofranco@hotmail.com>
2024-03-28 16:49:54 -0300

    Set up project initial organization and metadata

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin",
+    author="Ricardo Augusto Franco",
+    packages=find_packages(),
+)
