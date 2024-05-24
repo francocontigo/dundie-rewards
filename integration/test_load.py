bc2dc3e27c9a3bfab90a9494f5957cd7ac8adad9
francocontigo <ricardoaugustofranco@hotmail.com>
2024-05-24 12:52:59 -0300

    tes changes

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,10 +1,10 @@
 import pytest
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 
 
 @pytest.mark.integration
 @pytest.mark.medium
-def test_load():
+def test_load_postive_call_load_command():
     """Test command load"""
     out = (
         check_output(["dundie", "load", "tests/assets/people.csv"])
@@ -12,3 +12,13 @@ def test_load():
         .split("\n")
     )
     assert len(out) == 2
+
+@pytest.mark.integration
+@pytest.mark.medium
+def test_load_negative_call_load_command_with_wrong_params():
+    """Test command load"""
+    with pytest.raises(CalledProcessError) as error:
+        check_output(
+            ["dundie", "loady", "tests/assets/people.csv"]
+        ).decode("utf-8").split("\n")
+    assert "status 2" in str(error.getrepr())
