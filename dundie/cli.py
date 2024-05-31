d2875d8e8c6fb85c33bd41a348a4c3f3fb8b2736
Ricardo Franco <ricardoaugustofranco@hotmail.com>
2024-05-31 14:35:25 -0300

    implemented json database (#15)

--- dundie/cli.py
+++ dundie/cli.py
@@ -1,28 +1,44 @@
-import argparse
+import rich_click as click
+import pkg_resources
+from rich.table import Table
+from rich.console import Console
 
-from dundie.core import load  # noqa
+from dundie import core
+
+click.rich_click.USE_RICH_MARKUP = True
+click.rich_click.USE_MARKDOWN = True
+click.rich_click.SHOW_ARGUMENTS = True
+click.rich_click.GROUP_ARGUMENTS_OPTIONS = True
+click.rich_click.SHOW_METAVARS_COLUMN = False
+click.rich_click.APPEND_METAVARS_HELP = True
 
 
+@click.group()
+@click.version_option(pkg_resources.get_distribution("dundie").version)
 def main():
-    parser = argparse.ArgumentParser(
-        description="Dunder Mifflin Rewards CLI",
-        epilog="Enjoy and use with cautious.",
-    )
-    parser.add_argument(
-        "subcommand",
-        type=str,
-        help="The subcommand to run",
-        choices=("load", "show", "send"),
-        default=help,
-    )
-    parser.add_argument(
-        "filepath",
-        type=str,
-        help="File path to load",
-        default=None,
-    )
-    args = parser.parse_args()
-    try:
-        print(*globals()[args.subcommand](args.filepath))
-    except KeyError:
-        print("Subcommand is invalid")
+    """Dunder Mifflin Rewards Syste
+
+    This cli applicationi controls DM rewards.
+    """
+
+
+@main.command()
+@click.argument("filepath", type=click.Path(exists=True))
+def load(filepath):
+    """Load the file to the database.
+
+    - Validates data
+    - Parses the fle
+    - Loads to database
+    """
+    table = Table(title="Dunder Mifflin Associates")
+    headers = ["name", "dept", "role", "created", "e-mail"]
+    for header in headers:
+        table.add_column(header, style="magenta")
+
+    result = core.load(filepath)
+    for person in result:
+        table.add_row(*[str(value) for value in person.values()])
+
+    console = Console()
+    console.print(table)
